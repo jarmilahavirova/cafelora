4a8b94322404a0c3ddd9cb97a128a5f709289bde
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-04-27 13:30:51 +0200

    Obsah objednávky rozložen do komponent

---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,26 @@
+import { OrderItem } from "../OrderItem/OrderItem";
+import "./Order.css";
+
+export const Order = ({ items }) => (
+  <main className="order">
+    <div className="order__content container">
+      <h1>Vaše objedávnka</h1>
+      <p
+        className={
+          items.length === 0 ? "empty-order" : "empty-order empty-order--hide"
+        }
+      >
+        Zatím nemáte nic objednáno
+      </p>
+      <div className="order__items">
+        {items.map((item) => (
+          <OrderItem
+            key={item.id}
+            name={item.name}
+            image={`http://localhost:4000${item.image}`}
+          />
+        ))}
+      </div>
+    </div>
+  </main>
+);
--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -4,32 +4,19 @@ import "./index.css";
 import "./order.css";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
+import { Order } from "../components/Order/Order";
+
+const response = await fetch(
+  "http://localhost:4000/api/drinks?filter=ordered:eq:true&select=id,name,image"
+);
+const json = await response.json();
+const orderedList = json.data;
 
 document.querySelector("#root").innerHTML = render(
   <div className="page">
     <div className="page">
       <Header showMenu={false} />
-
-      <main className="order">
-        <div className="order__content container">
-          <h1>Vaše objedávnka</h1>
-          <p className="empty-order empty-order--hide">
-            Zatím nemáte nic objednáno
-          </p>
-          <div className="order__items">
-            <div className="order-item">
-              <img src="/cups/espresso.png" className="order-item__image" />
-              <div className="order-item__name">Espresso</div>
-            </div>
-
-            <div className="order-item">
-              <img src="/cups/doppio.png" className="order-item__image" />
-              <div className="order-item__name">Doppio</div>
-            </div>
-          </div>
-        </div>
-      </main>
-
+      <Order items={orderedList} />
       <Footer />
     </div>
   </div>
